5d2b735d1e49ff61c214af619d2d2f48e1ecfe22
sabrina <sabrina@blockinar.com>
2020-10-20 22:52:19 -0300

    estudiantes: ejercicios 4, 5 y 6

--- estudiantes.js
+++ estudiantes.js
@@ -26,4 +26,33 @@ const estudiantesConFamiliares = (familiares, arrayDeEstudiantes) => {
     })
 }
 
-console.table(estudiantesConFamiliares(["Sapo", "Lechuza"], estudiantes))
+// console.table(estudiantesConFamiliares(["Sapo", "Lechuza"], estudiantes))
+
+
+//4-obtenerPromedioDeEstudiante, que tome por parámetro une estudiante (que se saca del array estudiantes) 
+//devuelva el promedio total de todas las materias
+const obtenerPromedioDeEstudiante = (objetoEstudiante) => {
+    const promedio = objetoEstudiante.materias.reduce((acc, materia) => {
+        return acc + materia.promedio;
+    }, 0);
+    return promedio / objetoEstudiante.materias.length;
+}
+
+
+// 5. `estudiantesConPromedioMayorA`, que tome por parámetro un número y un array de estudiantes y devuelva un array con les estudiantes que tengan un promedio total mayor a dicho número (usar la función anterior)
+const estudiantesConPromedioMayorA = (numero, arrayEstudiantes) => {
+    return arrayEstudiantes.filter((estudiante) => obtenerPromedioDeEstudiante(estudiante) > numero)
+}
+
+// console.table(estudiantesConPromedioMayorA(6, estudiantes))
+
+// 6. `mejoresEstudiantesPorCasa`, que tome por parámetro el nombre de una casa y un array de estudiantes y devuelva les estudiantes de dicha casa cuyo promedio total es mayor a 6
+
+const mejoresEstudiantesPorCasa = (casa, arrayDeEstudiantes) => {
+    const porCasa = arrayDeEstudiantes.filter((estudiante) => estudiante.casa === casa)
+    console.log(porCasa) // 37
+
+    return estudiantesConPromedioMayorA(6, porCasa)
+}
+
+console.table(mejoresEstudiantesPorCasa('Hufflepuff', estudiantes))
